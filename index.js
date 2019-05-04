095aee43b871220972a57da31a3ceb5cf0f46c36
YuanL <laurali2016@outlook.com>
2019-05-03 23:14:23 -0400

    update imag

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // create a new variable called 'data'
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var dataset = [1,2,3,4,5];
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
@@ -18,16 +19,22 @@ var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+// adding scale to y    
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight]);
+
 // barchart
 var barChart = svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
+    // return svgHeight - d => return svgHeight - yScale(d) 
     .attr("y", function(d) {
-         return svgHeight - d 
+         return svgHeight - yScale(d) 
     })
     .attr("height", function(d) { 
-        return d; 
+        return yScale(d); 
     })
     .attr("width", barWidth - barPadding)
     .attr("class", "bar")
