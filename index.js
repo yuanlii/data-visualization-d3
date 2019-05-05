4087db5403ad279ad2f5b7421c7da9ba128a5b1c
YuanL <laurali2016@outlook.com>
2019-05-05 09:26:35 -0400

    add axes

--- index.js
+++ index.js
@@ -1,63 +1,42 @@
 
 // create a new variable called 'data'
-// var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-var dataset = [1,2,3,4,5];
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var svgWidth = 500, svgHeight = 300;
 var barWidth = (svgWidth / dataset.length);
-
-// using function
-// d3.select('body')
-//     .selectAll('p')
-//     .data(dataset)
-//     .enter()
-//     .append('p')
-//     .text('haha')
-//     .text(function(d){return d;});
 
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+// add scale to x    
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgWidth]);
+
 // adding scale to y    
 var yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset)])
-    .range([0, svgHeight]);
+    .range([0,svgHeight]);
 
-// barchart
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    // return svgHeight - d => return svgHeight - yScale(d) 
-    .attr("y", function(d) {
-         return svgHeight - yScale(d) 
-    })
-    .attr("height", function(d) { 
-        return yScale(d); 
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("class", "bar")
-    .attr("transform", function (d, i) {
-        var translate = [barWidth * i, 0]; 
-        return "translate("+ translate +")";
-    });
+// add x axis
+var x_axis = d3.axisBottom()
+    .scale(xScale);
 
-// barchart add labels
-var text = svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(function(d) {
-        return d;
-    })
-    .attr("y", function(d, i) {
-        return svgHeight - d - 2;
-    })
-    .attr("x", function(d, i) {
-        return barWidth * i;
-    })
-    .attr("fill", "#A64C38");
+// add y axis
+var y_axis = d3.axisLeft()
+    .scale(yScale);
+
+// append to svg element
+svg.append("g")
+    .attr("transform", "translate(50, 10)")
+    .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate  +")")
+    .call(x_axis);
 
 
 
