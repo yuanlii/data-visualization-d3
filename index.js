eaee4686b740532a9020ca2105c8ca5eef11cb97
YuanL <laurali2016@outlook.com>
2019-05-05 11:09:26 -0400

    add elem

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-
-var svgWidth = 500, svgHeight = 300;
-
-var svg = d3.select('svg')
+var svgWidth = 600, svgHeight = 500;
+var svg = d3.select("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .attr("class", "svg-container");
 
-// add scale to x    
-var xScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, svgWidth]);
+// append a line to svg
+// for line, we need to specify x1,y1, x2,y2 for the positions of the line
+// if it's horizontal line, y ticks remain the same; if vertical line, x ticks remain the same, and y ticks vary
+var line = svg.append("line")
+    .attr("x1", 100)
+    .attr("x2", 500)
+    .attr("y1", 50)
+    .attr("y2", 50)
+    // set the color of the line, using "stroke"
+    .attr("stroke", "red")
+    // set the width of the line, using "stroke-width"
+    .attr("stroke-width", 5);
 
-// adding scale to y    
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    // note: the order changes to make sure the value is increasing from the bottom level
-    .range([svgHeight - 30,0]);
+// append a rect to svg
+var rect = svg.append("rect")
+    // specify where to start drawing the rectangle
+    .attr("x", 100)
+    .attr("y", 100)
+    // specify width and height of the rectangle
+    .attr("width", 200)
+    .attr("height", 100)
+    // specify fill color
+    .attr("fill", "#9B95FF");
 
-// add x axis using axisBottom()
-var x_axis = d3.axisBottom()
-    .scale(xScale);
-
-// add y axis using axisLeft()
-var y_axis = d3.axisLeft()
-    .scale(yScale);
-
-// append group to svg element
-svg.append("g")
-    .attr("transform", "translate(50, 10)")
-    .call(y_axis);
-
-var xAxisTranslate = svgHeight - 20;
-
-svg.append("g")
-    .attr("transform", "translate(50, " + xAxisTranslate  +")")
-    .call(x_axis);
-
-
-
-
-
+// append a circle to svg
+var circle = svg.append("circle")
+    // set coordinates of the circle
+    .attr("cx", 200)
+    .attr("cy", 300)
+    // set radius
+    .attr("r", 80)
+    .attr("fill", "#7CE8D5"); 
+    
