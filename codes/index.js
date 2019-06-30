43f2e1671a339fc44e9c0ace29cb0c4018de7852
YuanL <laurali2016@outlook.com>
2019-06-30 00:12:40 -0700

    add append

--- codes/index.js
+++ codes/index.js
@@ -1,9 +1,9 @@
-function positionRects(d, i) {
-	return i * 40;
-  }
-
-function update() {
-	d3.selectAll('rect')
-		.attr('x', positionRects
-		);
-}
+d3.selectAll('g.item')
+  .append('text')
+// add text to each g.item
+  .text(function(d, i) {
+    return i + 1;
+  })
+//   specify the position of the item
+  	.attr('y', 50)
+	.attr('x', 30);
